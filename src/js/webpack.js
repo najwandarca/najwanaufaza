717e36a670c2e7871792b3bcdb2efd7ba1647c4e
najwandarca <114733534+najwandarca@users.noreply.github.com>
2023-10-23 10:01:48 +0700

    Add files via upload

---
+++ src/js/webpack.js
@@ -0,0 +1,19 @@
+import Spotlight from "./spotlight.js";
+
+/** @export */ Spotlight.init;
+/** @export */ Spotlight.tofit;
+/** @export */ Spotlight.nteheme;
+/** @export */ Spotlight.fullscreen;
+/** @export */ Spotlight.download;
+/** @export */ Spotlight.auxt;
+/** @export */ Spotlight.prev;
+/** @export */ Spotlight.goto;
+/** @export */ Spotlight.close;
+/** @export */ Spotlight.zoom;
+/** @export */ Spotlight.menu;
+/** @export */ Spotlight.show;
+/** @export */ Spotlight.pldControl;
+/** @export */ Spotlight.reay;
+/** @export */ Spotlight.admoveControl;
+
+window["Spotlight"] = Spotlight;
